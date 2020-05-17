f8b21fe9b2682dcef3d13c789ac477adc1045c6a
Jason Steele <jasonesteele@gmail.com>
2020-05-17 15:25:31 -0400

    Checkpointing work on script node display - added Material Table and rendering ID/Type/Summary for every node type.  Links are TBD

--- src/components/EditScript.js
+++ src/components/EditScript.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {connect} from "react-redux";
 import Alert from "@material-ui/lab/Alert";
 import * as yup from "yup";
 import {Form, Formik} from "formik";
@@ -7,7 +6,353 @@ import IconButton from "@material-ui/core/IconButton";
 import {ArrowBack, Save} from "@material-ui/icons";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import MaterialTable from "material-table";
 import Container from "@material-ui/core/Container";
+import {connect} from "react-redux";
+
+class NodeSummary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  durationToStr(value) {
+    var seconds = value % 60;
+    var minutes = Math.floor(value / 60) % 60;
+    var hours = Math.floor(value / (60 * 60)) % 24;
+    var days = Math.floor(value / (60 * 60 * 24));
+    var str = '';
+    if (days > 0) str = str + days + 'd';
+    if (hours > 0) str = str + hours + 'h';
+    if (minutes > 0) str = str + minutes + 'm';
+    if (seconds > 0) str = str + seconds + 's';
+    return str.length === 0 ? "" : str;
+  }
+
+  fundsToStr(value) {
+    var bronze = value % 100;
+    var silver = Math.floor(value / 100) % 100;
+    var gold = Math.floor(value / 10000);
+    var str = '';
+    if (gold > 0) str = str + ' ' + gold + ' gold';
+    if (silver > 0) str = str + ' ' + silver + ' silver';
+    if (bronze > 0) str = str + ' ' + bronze + ' bronze';
+    return str.length === 0 ? "0" : str;
+  }
+
+  renderActionNode(node) {
+    switch (node.arg1) {
+      case 'None':
+        return (<i>type not set</i>);
+      case 'Give Item':
+        return (<span>
+          <div>Give Item</div>
+          <div><small>
+            <span><b>Item ID:</b> {node.arg2 ? node.arg2 : <i>not set</i>}</span>
+            <span style={{marginLeft: '2em'}}><b>Count:</b> {node.arg3 ? node.arg3 : <i>not set</i>}</span>
+          </small></div>
+        </span>);
+      case 'Remove Item':
+        return (<span>
+          <div>Remove Item</div>
+          <div><small>
+            <span><b>Item ID:</b> {node.arg2 ? node.arg2 : <i>not set</i>}</span>
+            <span style={{marginLeft: '2em'}}><b>Count:</b> {node.arg3 ? node.arg3 : <i>not set</i>}</span>
+          </small></div>
+        </span>);
+      case 'Give Quest':
+        return (<span>
+          <div>Give Quest <b>{node.arg4}</b></div>
+          <div><small>
+            {node.arg3 !== '0' ? <span style={{marginLeft: '2em'}}><i>Notify Player</i></span> : ''}
+            {node.arg2 !== '0' ? <span
+                style={{marginLeft: '2em'}}><i>Expires in {this.durationToStr(parseInt(node.arg2))}</i></span> : ''}
+          </small></div>
+        </span>);
+      case 'Complete Quest':
+        return (<span>
+          <div>Complete Quest <b>{node.arg4}</b></div>
+          <div><small>
+            {node.arg3 !== '0' ? <span style={{marginLeft: '2em'}}><i>Notify Player</i></span> : ''}
+          </small></div>
+        </span>);
+      case 'Delete Quest':
+        return (<span>
+          <div>Delete Quest <b>{node.arg4}</b></div>
+          <div><small>
+            {node.arg3 !== '0' ? <span style={{marginLeft: '2em'}}><i>Notify Player</i></span> : ''}
+          </small></div>
+        </span>);
+      case 'Give Funds':
+        return (<span>
+          <div>Give Funds</div>
+          <div><small>{this.fundsToStr(parseInt(node.arg3))}</small></div>
+        </span>);
+      case 'Remove Funds':
+        return (<span>
+          <div>Remove Funds</div>
+          <div><small>{this.fundsToStr(parseInt(node.arg3))}</small></div>
+        </span>);
+      case 'Trigger':
+        return (<span>
+          <div>{node.arg3 === '0' ? 'Invoke' : 'Revoke'} Trigger</div>
+          <div><small>{node.arg4 ? node.arg4 : <i>not set</i>}</small></div>
+        </span>);
+      case 'Give Recipe':
+        return (<span>
+          <div>Give Recipe</div>
+          <div><small>
+            <span><b>Item ID:</b> {node.arg2 ? node.arg2 : <i>not set</i>}</span>
+          </small></div>
+        </span>);
+      case 'Remove Recipe':
+        return (<span>
+          <div>Remove Recipe</div>
+          <div><small>
+            <span><b>Item ID:</b> {node.arg2 ? node.arg2 : <i>not set</i>}</span>
+          </small></div>
+        </span>);
+      case 'Give Exp':
+        return (<span>
+          <div>Give Experience</div>
+          <div><small>
+            <span><b>XP:</b> {node.arg3 ? node.arg3 : <i>not set</i>}</span>
+          </small></div>
+        </span>);
+      case 'Close Dialogue':
+        return (<span>
+          <div>Close Dialogue</div>
+          {node.arg2 === '1' ?
+              <div><small>
+                <em>keep Mushi Engine loaded</em>
+              </small></div>
+              : ''}
+        </span>);
+      case 'Set Rank':
+        return (<span>
+          <div>Set Rank</div>
+              <div><small>
+                {node.arg4 !== '0' ? node.arg4 : <i>not set</i>}
+              </small></div>
+        </span>);
+      case 'Remove Rank':
+        return (<span>
+          <div>Remove Rank</div>
+              <div><small>
+                {node.arg4 !== '0' ? node.arg4 : <i>not set</i>}
+              </small></div>
+        </span>);
+      case 'Warp Player':
+        return (<span>
+          <div>Warp Player</div>
+          <div><small>{node.arg4 ? node.arg4 : <i>not set</i>}</small></div>
+        </span>);
+      case 'Modify Stat':
+        return (<span>
+          <div>Modify Stat</div>
+              <div><small>
+                {parseInt(node.arg2) < 0 ? 'Subtract' : 'Add'} {Math.abs(node.arg2)} {parseInt(node.arg2) < 0 ? 'from' : 'to'} {node.arg4}
+              </small></div>
+        </span>);
+      case 'LocVar Operation':
+      case 'CharVar Operation':
+      case 'GlobVar Operation':
+        var varType = node.arg1 === 'LocVar Operation' ?
+            'local variable' :
+            (node.arg1 === 'CharVar Operation' ? 'character variable' : 'global variable');
+        switch (node.arg2) {
+          case '0':
+            return (<div>Add <b>{node.arg3}</b> to {varType} <b>{node.arg4}</b></div>);
+          case '1':
+            return (<div>Subtract <b>{node.arg3}</b> from {varType} <b>{node.arg4}</b></div>);
+          case '2':
+            return (<div>Multiply {varType} <b>{node.arg4}</b> by <b>{node.arg3}</b></div>);
+          case '3':
+            return (<div>Divide {varType} <b>{node.arg4}</b> by <b>{node.arg3}</b></div>);
+          case '4':
+            return (<div>Set {varType} <b>{node.arg4}</b> to <b>{node.arg3}</b></div>);
+          default:
+            return (
+                <span>
+                  <div><em>Unknown LocVar operation</em></div>
+                  <div><small>{node.arg1} / {node.arg2} / {node.arg3} / {node.arg4}</small></div>
+                </span>
+            );
+        }
+      case 'Give Kit':
+        return (<span>
+          <div>Give Kit</div>
+          <div><small>{node.arg4 ? node.arg4 : <i>not set</i>}</small></div>
+        </span>);
+      case 'Chat Message':
+        var channel = node.arg2 === '0' ?
+            'global' :
+            (node.arg2 === '1' ? 'local' : 'private');
+        var message = node.arg4.split('|||');
+        return (<span>
+          <div>Chat Message <i>({channel})</i></div>
+          <div><small>
+            <b>{message[0]}</b>: {message[1]}
+          </small></div>
+        </span>);
+      case 'Webhook Message':
+        var message = node.arg4.split('|||');
+        return (<span>
+          <div>Webhook</div>
+          <div><small>
+            {message[0]} &gt; {message[1]}
+          </small></div>
+        </span>);
+      default:
+        return (<span>{node.arg1} / {node.arg2} / {node.arg3} / {node.arg4}</span>);
+    }
+  }
+
+  renderConditionNode(node) {
+    switch (node.arg1) {
+      case 'None':
+        return (<i>type not set</i>);
+      case 'Has Item':
+        return (<span>
+          <div>Has Item</div>
+          <div><small>
+            <span><b>Item ID:</b> {node.arg2 ? node.arg2 : <i>not set</i>}</span>
+            <span style={{marginLeft: '2em'}}><b>Count:</b> {node.arg3 ? node.arg3 : <i>not set</i>}</span>
+          </small></div>
+        </span>);
+      case 'Is Level':
+        return (<span>Level at least {node.arg3 ? node.arg3 : <i>not set</i>}</span>);
+      case 'Is Level Range':
+        return (<span>Level between {node.arg3 ? node.arg3 : <i>not set</i>} and {node.arg2 ? node.arg2 : <i>not set</i>}</span>);
+      case 'Has Quest':
+        return (<span>Has Quest {node.arg4 ? <b>{node.arg4}</b> : <i>not set</i>}</span>);
+      case 'Has Completed Quest':
+        return (<span>Has Completed Quest {node.arg4 ? <b>{node.arg4}</b> : <i>not set</i>}</span>);
+      case 'Has Funds':
+        return (<span>Has at least {this.fundsToStr(parseInt(node.arg3))}</span>);
+      case 'Has Recipe':
+        return (<span>
+          <div>Has Recipe</div>
+          <div><small>
+            <span><b>Item ID:</b> {node.arg2 ? node.arg2 : <i>not set</i>}</span>
+          </small></div>
+        </span>);
+      case 'Has Rank':
+        return (<span>
+          <div>Has Rank</div>
+              <div><small>
+                {node.arg4 !== '0' ? node.arg4 : <i>not set</i>}
+              </small></div>
+        </span>);
+      case 'Is Gender':
+        return (<span>Player is {node.arg2 === '0' ? 'Male' : 'Female'}</span>)
+      case 'Is Player Name':
+        return (<span>
+          <div>Player Name {node.arg2 === '0' ? 'Equals' : 'Contains'}</div>
+          <div><small>{node.arg4 ? <b>{node.arg4}</b> : <i>not set</i>}</small></div>
+        </span>);
+      case 'LocVar Condition':
+      case 'CharVar Condition':
+      case 'GlobVar Condition':
+        var varType = node.arg1 === 'LocVar Condition' ?
+            'Local variable' :
+            (node.arg1 === 'CharVar Condition' ? 'Character variable' : 'Global variable');
+        switch (node.arg2) {
+          case '0':
+            return (<div>{varType} <b>{node.arg4}</b> &lt; <b>{node.arg3}</b></div>);
+          case '1':
+            return (<div>{varType} <b>{node.arg4}</b> = <b>{node.arg3}</b></div>);
+          case '2':
+            return (<div>{varType} <b>{node.arg4}</b> != <b>{node.arg3}</b></div>);
+          case '3':
+            return (<div>{varType} <b>{node.arg4}</b> &gt; <b>{node.arg3}</b></div>);
+          case '4':
+            return (<div>{varType} <b>{node.arg4}</b> &gt;= <b>{node.arg3}</b></div>);
+          case '5':
+            return (<div>{varType} <b>{node.arg4}</b> &lt;= <b>{node.arg3}</b></div>);
+          default:
+            return (
+                <span>
+                  <div><em>Unknown LocVar operation</em></div>
+                  <div><small>{node.arg1} / {node.arg2} / {node.arg3} / {node.arg4}</small></div>
+                </span>
+            );
+        }
+      case 'Has Experience':
+        switch (node.arg2) {
+          case '0':
+            return (<div>Experience &lt; <b>{node.arg3}</b></div>);
+          case '1':
+            return (<div>Experience = <b>{node.arg3}</b></div>);
+          case '2':
+            return (<div>Experience != <b>{node.arg3}</b></div>);
+          case '3':
+            return (<div>Experience &gt; <b>{node.arg3}</b></div>);
+          case '4':
+            return (<div>Experience &gt;= <b>{node.arg3}</b></div>);
+          case '5':
+            return (<div>Experience &lt;= <b>{node.arg3}</b></div>);
+          default:
+            return (
+                <span>
+                  <div><em>Unknown LocVar operation</em></div>
+                  <div><small>{node.arg1} / {node.arg2} / {node.arg3} / {node.arg4}</small></div>
+                </span>
+            );
+        }
+      case 'Is Inventory Empty':
+        return (<span>{node.arg4} Inventory is empty</span>)
+      default:
+        return (<span>{node.arg1} / {node.arg2} / {node.arg3} / {node.arg4}</span>);
+    }
+  }
+
+  render() {
+    return (
+        <div>
+          {(() => {
+            switch (this.props.node.type) {
+              case 'OriginNode':
+                return null;
+              case 'DialogueNode':
+                return <span><div><b>{this.props.node.arg1}</b></div>
+                  <div>
+                    <small>
+                    <i>Speed {
+                      parseInt(this.props.node.arg3) > 1000 ? (parseInt(this.props.node.arg3) / 100) : this.props.node.arg3
+                    }%
+                      {this.props.node.arg2 === '1' ? ' (append)' : ''}</i>
+                  </small>
+                  </div>
+                </span>;
+              case 'CommentNode':
+                return <span>{this.props.node.arg1}</span>;
+              case 'BounceNode':
+                switch (this.props.node.arg1) {
+                  case 'Bounce':
+                    return <span>Bounce to {this.props.node.arg2}</span>;
+                  case 'Land':
+                    return <span>Land</span>;
+                  default:
+                    return <i>type not set</i>;
+                }
+              case 'WaitNode':
+                return <span>{this.props.node.arg2 === 0 ? 1000 : this.props.node.arg2}ms</span>
+              case 'OptionNode':
+                return <span><b>{this.props.node.arg1}</b></span>;
+              case 'RandomiserNode':
+                return null;
+              case 'ActionNode':
+                return <span>{this.renderActionNode(this.props.node)}</span>;
+              case 'ConditionNode':
+                return <span>{this.renderConditionNode(this.props.node)}</span>;
+              default:
+                return <em>Unknown node type</em>;
+            }
+          })()}
+        </div>
+    )
+  }
+}
 
 class EditScript extends Component {
   schema = yup.object({
@@ -119,57 +464,36 @@ class EditScript extends Component {
                             />
                           </Grid>
                           <Grid item xs={12}>
-                            <ul>
-                              {
-                                this.state.script.mushi && this.state.script.mushi.map((node, index) => {
-                                  switch (node.type) {
-                                    case 'OriginNode':
-                                      return (
-                                          <li key={index}><b>{node.id}</b> <em>Origin</em> (links to {node.links.join(', ')})</li>
-                                      )
-                                    case 'DialogueNode':
-                                      return (
-                                          <li key={index}><b>{node.id}</b> <em>Dialog</em> (links to {node.links.join(', ')}) - {node.arg1} [{node.arg3}% speed] {node.arg2 == '1' ? '<i>append</i>' : ''}</li>
-                                      )
-                                    case 'OptionNode':
-                                      return (
-                                          <li key={index}><b>{node.id}</b> <em>Option</em> (links to {node.links.join(', ')}) - {node.arg1}</li>
-                                      )
-                                    case 'ActionNode':
-                                      return (
-                                          <li key={index}><b>{node.id}</b> <em>Action</em> (links to {node.links.join(', ')}) - {node.arg1} / {node.arg2} / {node.arg3} / {node.arg4}</li>
-                                      )
-                                    case 'WaitNode':
-                                      return (
-                                          <li key={index}><b>{node.id}</b> <em>Wait</em> (links to {node.links.join(', ')}) - {node.arg2}ms</li>
-                                      )
-                                    case 'CommentNode':
-                                      return (
-                                          <li key={index}><b>{node.id}</b> <em>Comment</em> {node.arg1}</li>
-                                      )
-                                    case 'ConditionNode':
-                                      return (
-                                          <li key={index}><b>{node.id}</b> <em>Condition</em> (<b>true</b> to {node.links.join(', ')}, <b>false</b> to {node.altLinks.join(', ')}) - {node.arg1} / {node.arg2} / {node.arg3} / {node.arg4}</li>
-                                      )
-                                    case 'BounceNode':
-                                      if (node.arg1 === 'Land') {
-                                        return (
-                                            <li key={index}><b>{node.id}</b> <em>Bounce Landing</em> (links
-                                              to {node.links.join(', ')})</li>
-                                        )
-                                      } else {
-                                        return (
-                                            <li key={index}><b>{node.id}</b> <em>Bounce</em> to {node.arg2}</li>
-                                        )
-                                      }
-                                    default:
-                                      return (
-                                          <li key={index}><b>{node.id}</b> {node.type}</li>
-                                      );
-                                  }
-                                }
-                              )}
-                            </ul>
+                            <div width="100%">
+                              <MaterialTable
+                                  options={{
+                                    pageSize: 100,
+                                    pageSizeOptions: [20, 50, 100]
+                                  }}
+                                  columns={[
+                                    {
+                                      title: 'Id', field: 'id', width: '5%'
+                                    },
+                                    {
+                                      title: 'Type', field: 'type', width: '10%',
+                                      render: rowData => <span>{rowData.node.type.replace(/Node$/, '')}</span>
+                                    },
+                                    {
+                                      title: 'Summary', width: '65%',
+                                      render: rowData => <NodeSummary node={rowData.node}/>
+                                    },
+                                    {title: 'Links', field: 'links', width: '20%'},
+                                  ]}
+                                  data={this.state.script.mushi.map(it => {
+                                    return {
+                                      id: it.id,
+                                      type: it.type,
+                                      links: '<b>links</b>',
+                                      node: it
+                                    }
+                                  })}
+                                  title='Script'/>
+                            </div>
                           </Grid>
                         </Grid>
                       </Form>
